4f007027ffee360bcc49bd66f4df97eb8668a0b2
Cristian Ginarte Cardenas <crisgtx18@gmail.com>
2026-09-03 13:58:42 -0300

    Changes

---
+++ Modulo-02/scripts_practice.js
@@ -0,0 +1,6 @@
+var name = "Cristian";
+var age = 30;
+var isStudent = true;
+var hobbies = ["gaming", "music", "coding"];
+
+alert("Name: " + name + "\nAge: " + age + "\nIs Student: " + isStudent + "\nHobbies: " + hobbies.join(", "));
